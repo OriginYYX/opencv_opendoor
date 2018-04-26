04cdce132f657053cfdb83b1da9fbe9cf09c1a70
OriginYYX <oringin@icloud.com>
2018-04-26 21:09:04 +0800

    get_token

--- face_match.py
+++ face_match.py
@@ -7,19 +7,21 @@ Created on Wed Apr 25 21:36:23 2018
 
 import cv2
 import base64
-import urllib.request
+import requests
 
 API_KEY = 'vVsNOqLoB1QwwyVITZaPswHq'  
 SECRET_KEY = 'ub0n4T1oqGQ6NELSwm3kV2niN3EtYws9'
 url = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=vVsNOqLoB1QwwyVITZaPswHq&client_secret=ub0n4T1oqGQ6NELSwm3kV2niN3EtYws9'
-headers={'Content-Type', 'application/json; charset=UTF-8'}
-request=urllib.request.Request(url,headers=headers)
-response = urllib.request.urlopen(request)
-content = response.read()
-if (content):
-    print(content)
+
+
+def get_token():
+    response = requests.post(url)
+    
+    return response.json()['access_token']
     
 def pic_base64(filePath):
     with open(filePath, 'rb') as fp:  
         return base64.b64encode(fp.read)
         
+if __name__ == '__main__':
+    print(get_token())
